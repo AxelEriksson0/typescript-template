ed497adb60eaa982ea17d8151e81e7924aeda1c9
Axel Eriksson <axeleriksson0@gmail.com>
2021-12-15 23:55:37 +0100

    Adding a few new rules

--- src/spec/index.spec.ts
+++ src/spec/index.spec.ts
@@ -1,11 +1,11 @@
 import { addTwoNumbers } from "../index"
 
-describe('addTwoNumbers', () => {
+describe("addTwoNumbers", () => {
 
-    it('addTwoNumbers(5, 10) should return 15', () => {
+    it("addTwoNumbers(5, 10) should return 15", () => {
         const result = addTwoNumbers(5, 10)
         if (result !== 15) {
-            throw Error('Result was not 15, waiting for expect')
+            throw Error("Result was not 15, waiting for expect")
         }
     })
 
