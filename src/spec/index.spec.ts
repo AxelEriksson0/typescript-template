9ed2ed0f95cd98cc716b03cb33493cb84a363e85
Axel Eriksson <axeleriksson0@gmail.com>
2022-04-01 22:57:21 +0200

    Adding more strict checks and replacing mocha with jest

--- src/spec/index.spec.ts
+++ src/spec/index.spec.ts
@@ -2,7 +2,7 @@ import { addTwoNumbers } from "../index"
 
 describe("addTwoNumbers", () => {
 
-    it("addTwoNumbers(5, 10) should return 15", () => {
+    test("addTwoNumbers(5, 10) should return 15", () => {
         const result = addTwoNumbers(5, 10)
         if (result !== 15) {
             throw Error("Result was not 15, waiting for expect")
