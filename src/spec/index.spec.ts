7be4c5349896d2040e19098de6b1eca138116de7
Axel Eriksson <axeleriksson0@gmail.com>
2024-03-04 01:07:29 +0100

    Update dependencies

--- src/spec/index.spec.ts
+++ src/spec/index.spec.ts
@@ -3,8 +3,6 @@ import { expect, test } from "vitest"
 import { addTwoNumbers } from "../utils/addTwoNumbers.js"
 import { multiplyTwoNumbers } from "../utils/multiplyTwoNumbers.js"
 
-// describe("Mathematical operations", () => {
-
 test("addTwoNumbers(5, 10) should return 15", () => {
   const result = addTwoNumbers(5, 10)
   expect(result).toBe(15)
