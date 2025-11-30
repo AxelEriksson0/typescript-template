15b75cd2e25591a2ebd740b45b9115e7bcd35b46
Axel Eriksson <axeleriksson0@gmail.com>
2025-11-30 17:07:48 +0100

    Update App.tsx and add .cursor directory

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,8 +5,8 @@ export const App = () => {
   return (
     <>
       <Navbar />
-      <div class="container mx-auto">
-        <p class="pt-8 text-3xl">{m.home_page_title()}</p>
+      <div class="container mx-auto px-3">
+        <p class="pt-6 text-3xl mb-4">{m.home_page_title()}</p>
       </div>
     </>
   )
