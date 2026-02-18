adc79c03277b7fce3a12c1c4798d0f7e69098423
Axel Eriksson <axeleriksson0@gmail.com>
2026-02-18 22:16:45 +0100

    Fix oxlint vscode extension

--- packages/web/src/components/LanguageToggle/LanguageToggle.tsx
+++ packages/web/src/components/LanguageToggle/LanguageToggle.tsx
@@ -10,14 +10,14 @@ export const LanguageToggle = () => {
       </summary>
       <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
         <li>
-          <a onClick={() => setLocale("en")} onKeyDown={() => setLocale("en")}>
+          <button onClick={() => setLocale("en")} onKeyDown={() => setLocale("en")}>
             English
-          </a>
+          </button>
         </li>
         <li>
-          <a onClick={() => setLocale("sv")} onKeyDown={() => setLocale("en")}>
+          <button onClick={() => setLocale("sv")} onKeyDown={() => setLocale("sv")}>
             Svenska
-          </a>
+          </button>
         </li>
       </ul>
     </details>
